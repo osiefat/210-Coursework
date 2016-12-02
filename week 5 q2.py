059448e6a954007c90892a94f2ab212bd5baddee
osiefat <t.osiefa@yahoo.com>
2016-12-02 03:43:05 +0000

    Add files via upload
    Week 5 Question 2

---
+++ week 5 q2.py
@@ -0,0 +1,49 @@
+class Node(object):
+    def __init__(self, value):
+        self.value=value
+        self.next=None
+        self.prev=None
+
+class List(object):
+    def __init__(self):
+        self.head=None
+        self.tail=None
+          
+    def insert(self,n,x):
+        if n!=None:
+            x.next=n.next
+            n.next=x
+            x.prev=n
+            if x.next!=None:
+                x.next.prev=x              
+        if self.head==None:
+            self.head=self.tail=x
+            x.prev=x.next=None
+        elif self.tail==n:
+            self.tail=x
+              
+    def remove(self, n): # remove function 
+        if n.prev==0: # if n.prev==0, it beomes n.next
+            n.prev.next=n.next
+        else:
+            self.head=n.next
+        if n.next==0:
+            n.next.prev=n.prev
+        else:
+            self.tail=n.prev
+              
+    def display(self):
+        values=[]
+        n=self.head
+        while n!=None:
+            values.append(str(n.value))
+            n=n.next
+        print("List: ",",".join(values))
+         
+if __name__ == '__main__':
+    l=List()
+    l.insert(None, Node(4))
+    l.insert(l.head,Node(6))
+    l.insert(l.head,Node(8))
+    l.remove(l.head) #added the remove node "4"
+    l.display()
