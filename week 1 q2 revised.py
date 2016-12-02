3ec74cc9d16e0db89e373fb7ce9b8867d9f561ca
osiefat <t.osiefa@yahoo.com>
2016-12-02 03:31:27 +0000

    Add files via upload
    Week 1 Question 2

---
+++ week 1 q2 revised.py
@@ -0,0 +1,15 @@
+inp=10#1#
+t=5#1#
+zero=0 #1
+while t<=inp:#n#
+    zero+=(inp//t)#n#
+    t*=5#n#
+    print(t)#n#
+
+
+#Big O Notation O(n)
+# While t is less than or equal to zero it adds the sum of inp floor divided by t 
+
+
+
+
